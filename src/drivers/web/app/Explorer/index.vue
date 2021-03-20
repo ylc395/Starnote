822923e134ccaa83888b2f77eb8c18981ad565b6
ylc395 <cyl@cyl.moe>
2021-03-20 18:39:06 +0800

    remove invalid code

--- src/drivers/web/app/Explorer/index.vue
+++ src/drivers/web/app/Explorer/index.vue
@@ -3,14 +3,12 @@ import { defineComponent, provide } from 'vue';
 import TreeViewer from './TreeViewer/index.vue';
 import NoteList from './NoteList/index.vue';
 import { NotebookTreeService, token } from 'domain/service/NotebookTreeService';
-import { ContextmenuService } from 'drivers/web/components/Contextmenu/contextmenu.service';
 import { selfish } from 'utils/index';
 
 export default defineComponent({
   components: { TreeViewer, NoteList },
   setup() {
     provide(token, selfish(new NotebookTreeService()));
-    ContextmenuService.setup();
   },
 });
 </script>
