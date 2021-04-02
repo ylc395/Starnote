30871da56a6acbffd2f0cc791d3ae8ffe3fd4c8b
ylc395 <cyl@cyl.moe>
2021-04-02 16:10:47 +0800

    add markdown highlight

--- src/drivers/web/app/EditorArea/Editor/codemirrorEditor.ts
+++ src/drivers/web/app/EditorArea/Editor/codemirrorEditor.ts
@@ -3,12 +3,17 @@ import { Editor } from 'domain/entity';
 import { EditorView, ViewUpdate, keymap } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
 import { defaultKeymap } from '@codemirror/commands';
+import { markdown } from '@codemirror/lang-markdown';
+import { defaultHighlightStyle } from '@codemirror/highlight';
 
 export function codemirrorEditor(editor: Editor, el: HTMLElement) {
   return new EditorView({
+    parent: el,
     state: EditorState.create({
       doc: editor.content.value,
       extensions: [
+        markdown(),
+        defaultHighlightStyle,
         keymap.of(defaultKeymap),
         EditorView.updateListener.of((v: ViewUpdate) => {
           if (v.docChanged) {
@@ -17,6 +22,5 @@ export function codemirrorEditor(editor: Editor, el: HTMLElement) {
         }),
       ],
     }),
-    parent: el,
   });
 }
